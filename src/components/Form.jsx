f2f7b6716e094205841d4edcdc3753599fff73fc
POOL4T7 <abhinavg90834@gmail.com>
2023-08-23 23:01:24 +0530

    dynamic all possible path

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Box, Button, Input, FormControl, FormLabel } from "@chakra-ui/react";
+import { Box, Input, FormControl, FormLabel } from "@chakra-ui/react";
 import { MazeContext } from "../context/maze";
 // import Matrix from './Matrix';
 
@@ -17,39 +17,27 @@ function MazeForm() {
     setColumns(parseInt(e.target.value || 0));
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // You can handle the form submission here, e.g., update the matrix state with the blocked index.
-    // setPaths(findPath(rows, columns))
-  };
-
   return (
     <Box p={4}>
-      <form onSubmit={handleSubmit}>
-        <FormControl>
-          <FormLabel htmlFor="rows">Rows:</FormLabel>
-          <Input
-            type="number"
-            id="rows"
-            value={rows}
-            onChange={handleRowsChange}
-          />
-        </FormControl>
+      <FormControl>
+        <FormLabel htmlFor="rows">Rows:</FormLabel>
+        <Input
+          type="number"
+          id="rows"
+          value={rows}
+          onChange={handleRowsChange}
+        />
+      </FormControl>
 
-        <FormControl mt={2}>
-          <FormLabel htmlFor="columns">Columns:</FormLabel>
-          <Input
-            type="number"
-            id="columns"
-            value={columns}
-            onChange={handleColumnsChange}
-          />
-        </FormControl>
-
-        <Button mt={4} colorScheme="teal" type="submit">
-          Generate path
-        </Button>
-      </form>
+      <FormControl mt={2}>
+        <FormLabel htmlFor="columns">Columns:</FormLabel>
+        <Input
+          type="number"
+          id="columns"
+          value={columns}
+          onChange={handleColumnsChange}
+        />
+      </FormControl>
     </Box>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,9 @@
 import Hero from "../components/Hero";
-// import Newsletter from '../components/Newsletter'
-import MazeForm from "../components/Form";
-import Matrix from "../components/Matrix";
 
 const Home = () => {
   return (
     <>
       <Hero />
-      {/* <Newsletter /> */}
-      <Matrix rows={4} columns={4} blockedIndex={'0,0'} />
-      <MazeForm />
     </>
   );
 };
